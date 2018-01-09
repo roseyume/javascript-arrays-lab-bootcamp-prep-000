fd8800d5ed93a0eb784b331061f9f4bfbaf6e8f1
Rosie Wang <rosiedwang@yahoo.com>
2018-01-09 21:48:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -27,10 +27,10 @@ function prependKitten(name)
 }
 function removeLastKitten()
 {
-  return kittens.slice(-kittens.length-2)
+  return kittens.slice(0,kittens.length-2)
   
 }
 function removeFirstKitten()
 {
-  return kittens.slice(kittens.length)
+  return kittens.slice(-kittens.length-2)
 }
